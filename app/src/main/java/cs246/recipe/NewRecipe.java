7593de6843a7b497ef795f7786a2738607238cce
eis16004 <eis16004@byui.edu>
2018-03-07 14:08:25 -0700

    Changed text from main file from hardcoded to @string

--- app/src/main/java/cs246/recipe/NewRecipe.java
+++ app/src/main/java/cs246/recipe/NewRecipe.java
@@ -3,12 +3,18 @@ package cs246.recipe;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class NewRecipe extends AppCompatActivity {
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_recipe);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
 }
