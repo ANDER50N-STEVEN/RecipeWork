433558a1654f09a5c7266866629263c9854d36db
ankitadev <ankitae.dev@gmail.com>
2018-03-19 16:16:52 -0400

    Main Activity Login page/Ankita/3/19/2018

--- app/src/main/java/cs246/recipe/MainActivity.java
+++ app/src/main/java/cs246/recipe/MainActivity.java
@@ -3,64 +3,95 @@ package cs246.recipe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import java.sql.Ref;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private StorageReference mStorageRef;
-    private TextView mTextMessage;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-    @Override
+    private static final String TAG = "EmailPassword";
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLoginBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = findViewById(R.id.message);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mAuth = FirebaseAuth.getInstance();
+        mEmailField = (EditText) findViewById(R.id.emailInput);
+        mPasswordField = (EditText) findViewById(R.id.passwordInput);
+        mLoginBtn = (Button) findViewById(R.id.loginButton);
+/*
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth mAuth) {
 
-        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+                if(mAuth.getCurrentUser() != null)
+                {
+                    Toast.makeText(MainActivity.this, "Going to another page!", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(MainActivity.this, Welcome.class));
+                }
+            }
+        };*/
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignIn();
+            }
+        });
     }
 
-    public void switchToCookbook(View view) {
-        Intent intent = new Intent(this, CookBookActivity.class);
-        startActivity(intent);
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //mAuth.addAuthStateListener(mAuthListener);
     }
 
-    public void switchToShoppingList(View view) {
-        Intent intent = new Intent(this, ShoppingListActivity.class);
-        startActivity(intent);
+    private void startSignIn()
+    {
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password))
+        {
+            Toast.makeText(MainActivity.this, "Fields are empty!", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                       Toast.makeText(MainActivity.this, "Sign in Successful!", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(MainActivity.this, Welcome.class));
+                    }
+                    else
+                    {
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
-    public void switchToPantry(View view) {
-        Intent intent = new Intent(this, PantryActivity.class);
-        startActivity(intent);
-    }
 
 }
--- app/src/main/java/cs246/recipe/Recipe.java
+++ app/src/main/java/cs246/recipe/Recipe.java
@@ -1,8 +1,16 @@
 package cs246.recipe;
 
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+//import com.firebase.client.Firebase;
+
 public class Recipe extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+       // Firebase.setAndroidContext(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
